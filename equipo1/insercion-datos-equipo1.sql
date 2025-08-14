89225c8dd0480d7563eeed2e6129c245692b3d6f
KevinBalvin03 <167731460+KevinBalvin03@users.noreply.github.com>
2025-08-14 01:48:30 -0500

    Update insercion-datos-equipo1.sql

--- equipo1/insercion-datos-equipo1.sql
+++ equipo1/insercion-datos-equipo1.sql
@@ -9,4 +9,7 @@ VALUES
 ('Dr. Miguel Herrera', 'miguel.herrera@zenith.com', 'hash101', 'Professional'),
 ('Lucía Fernández', 'lucia.fernandez@zenith.com', 'hash202', 'Patient'),
 ('Ana Torres', 'ana.torres@zenith.com', 'hash303', 'Patient'),
-('Javier Morales', 'javier.morales@zenith.com', 'hash404', 'Moderator');
+('Javier Morales', 'javier.morales@zenith.com', 'hash404', 'Moderator'),
+('Dr. Andrés López', 'andres.lopez@zenith.com', 'hash505', 'Professional'),
+('Dra. Sofía Martínez', 'sofia.martinez@zenith.com', 'hash606', 'Professional'),
+('Dr. Pablo Castillo', 'pablo.castillo@zenith.com', 'hash707', 'Professional');
