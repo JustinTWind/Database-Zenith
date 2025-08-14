027eb1df333d9a21bb295e50f88b6541c4997cf8
KevinBalvin03 <167731460+KevinBalvin03@users.noreply.github.com>
2025-08-14 02:09:17 -0500

    Update ConsultasEquipo2.sql

--- equipo2/ConsultasEquipo2.sql
+++ equipo2/ConsultasEquipo2.sql
@@ -11,8 +11,12 @@ JOIN ProfessionalType pt ON pr.TypeID = pt.TypeID
 WHERE pr.ProfessionalID = 4;
 
 --HU048C5 LISTAR PROFESIONALES Y SUS PROYECTOS
-select p.ProfessionalID, u.FullName, pt.[Name], y.[Name], y.[Year], y.[Description] from Professional p
+select p.ProfessionalID as Id_Professional, u.FullName as [Name], 
+pt.[Name] as Professional_Type, count(j.ProjectID) as Cant_Projects
+from Professional p
 inner join [User] u on  p.UserID = u.UserID
-inner join project y on y.ProfessionalID = p.ProfessionalID
+inner join project j on j.ProfessionalID = p.ProfessionalID
 inner join ProfessionalType pt on p.TypeID = pt.TypeID
-where u.FullName LIKE 'Dr%'
+where u.UserType = 'Professional'
+group by p.ProfessionalID,u.FullName,pt.[Name]
+order by Cant_Projects asc;
